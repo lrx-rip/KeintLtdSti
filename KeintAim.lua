2e255033d9d7121dfbdb6c40dd8371baf25fba3b
lrx.py <68545114+lrx-rip@users.noreply.github.com>
2024-10-19 03:44:29 +0300

    Update KeintAim.lua

--- KeintAim.lua
+++ KeintAim.lua
@@ -158,7 +158,7 @@ end
 
 local UserInputService = game:GetService("UserInputService")
 UserInputService.InputBegan:Connect(function(input)
-    if input.KeyCode == Enum.KeyCode.Q then
+    if input.KeyCode == Enum.KeyCode.V then
         toggleAimbot()
     end
 end)
