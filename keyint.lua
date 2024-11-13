afc6a7dbb47131b19619cefc17f48c0e8e250624
lrx.py <68545114+lrx-rip@users.noreply.github.com>
2024-11-13 21:36:17 +0300

    Add files via upload

---
+++ keyint.lua
@@ -0,0 +1,43 @@
+local library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/lrx-rip/KeintLtdSti/main/KeintMain.lua"))()
+
+local Main = library:CreateWindow("KEINT GNG [LrX & HakoKeint]","Crimson")
+
+local tab = Main:CreateTab("AimSegs")
+
+local tab3 = Main:CreateTab("Oyuncu")
+local tab4 = Main:CreateTab("Tehlikeli Mekan")
+
+local tab5 = Main:CreateTab("Kurucu İletişim")
+
+tab:CreateButton("AimBot",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lrx-rip/KeintLtdSti/main/KeintAim.lua"))()
+end)
+
+tab:CreateButton("Hitsegs",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lrx-rip/KeintLtdSti/refs/heads/main/AimKeint.lua"))()
+end)
+
+tab3:CreateSlider("Hız",16,555,function(a)
+    game:GetService("RunService").RenderStepped:wait(0)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = a
+end)
+
+tab4:CreateButton("HagoGeyint☠️",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lrx-rip/KeintLtdSti/main/KeintGodMode.lua"))()
+end)
+
+tab4:CreateButton("☠️ 4380 ☠️",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lrx-rip/KeintLtdSti/refs/heads/main/rpgkeyint.lua"))()
+end)
+
+tab5:CreateButton("Telegram",function()
+    setclipboard('https://t.me/keintcheck')
+    
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Başarılı!";
+        Text = "Telegram Linki Kopyalandı.";
+        Duration = 4;
+    })
+end)
+
+tab:Show()
